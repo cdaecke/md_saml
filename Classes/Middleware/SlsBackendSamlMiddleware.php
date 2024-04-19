1c18a7ebba403ee1c86a6ed501058478cba29a9c
chris <typo3@mediadreams.org>
2024-04-19 15:25:48 +0200

    [TASK] implementing the Single Logout Service
    references #27

---
+++ Classes/Middleware/SlsBackendSamlMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Extension "md_saml" for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2022 Christoph Daecke <typo3@mediadreams.org>
+ */
+
+namespace Mediadreams\MdSaml\Middleware;
+
+use OneLogin\Saml2\Error;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Class SlsFrontendSamlMiddleware
+ */
+class SlsFrontendSamlMiddleware extends SlsSamlMiddleware
+{
+    /**
+     * Process request
+     *
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     * @throws Error
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $this->context = 'BE';
+        return parent::process($request, $handler);
+    }
+}
