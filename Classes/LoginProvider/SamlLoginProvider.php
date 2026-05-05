7050ea7f4aa92cad2fe3155b66013a02dabd61eb
chris <typo3@mediadreams.org>
2026-05-05 15:45:35 +0200

    [FEATURE] TYPO3 v14 compatibility (dual v13+v14 support)

--- Classes/LoginProvider/SamlLoginProvider.php
+++ Classes/LoginProvider/SamlLoginProvider.php
@@ -13,24 +13,45 @@ declare(strict_types=1);
 
 namespace Mediadreams\MdSaml\LoginProvider;
 
+use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\Controller\LoginController;
 use TYPO3\CMS\Backend\LoginProvider\LoginProviderInterface;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\View\ViewInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3Fluid\Fluid\View\TemplateAwareViewInterface;
 
 class SamlLoginProvider implements LoginProviderInterface
 {
-    /**
-     * Renders the login fields
-     *
-     * @param StandaloneView $view
-     * @param PageRenderer $pageRenderer
-     * @param LoginController $loginController
-     */
+    public function modifyView(ServerRequestInterface $request, ViewInterface $view): string
+    {
+        // getRenderingContext() is declared on TemplateAwareViewInterface in Fluid 5.x (v14),
+        // but not in Fluid 4.x (v13). The concrete class has the method in both versions.
+        if ($view instanceof TemplateAwareViewInterface) {
+            // @phpstan-ignore-next-line
+            $view->getRenderingContext()->getTemplatePaths()->setTemplatePathAndFilename(
+                GeneralUtility::getFileAbsFileName('EXT:md_saml/Resources/Private/Templates/Backend/LoginSaml.html')
+            );
+        }
+
+        $queryParams = $request->getQueryParams();
+        if (isset($queryParams['error']) && $queryParams['error'] !== '') {
+            $view->assign('loginError', true);
+        }
+
+        $view->assign('enablePasswordReset', false);
+        return '';
+    }
+
+    // v13 compatibility: StandaloneView does not exist in v14, but this method is never
+    // called there (lazy type resolution). StandaloneView does not implement
+    // TYPO3\CMS\Core\View\ViewInterface, so this must remain self-contained.
     public function render(StandaloneView $view, PageRenderer $pageRenderer, LoginController $loginController): void
     {
-        $view->getRenderingContext()->getTemplatePaths()->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:md_saml/Resources/Private/Templates/Backend/LoginSaml.html'));
+        $view->getRenderingContext()->getTemplatePaths()->setTemplatePathAndFilename(
+            GeneralUtility::getFileAbsFileName('EXT:md_saml/Resources/Private/Templates/Backend/LoginSaml.html')
+        );
 
         $queryParams = ($GLOBALS['TYPO3_REQUEST'] ?? null)?->getQueryParams() ?? [];
         if (isset($queryParams['error']) && $queryParams['error'] !== '') {
